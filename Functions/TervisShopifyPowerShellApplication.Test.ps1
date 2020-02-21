9c5a02f451263d29c2788196bf28d719a36b6277
Hamilton Perez <hambone124@users.noreply.github.com>
2020-02-21 09:06:07 -0500

    Get-TervisShopifyEBSRecord

--- Functions/TervisShopifyPowerShellApplication.Test.ps1
+++ Functions/TervisShopifyPowerShellApplication.Test.ps1
@@ -36,3 +36,18 @@ function Get-TervisShopifyEndlessAisleTestOrder {
     )
     Get-ShopifyOrders -ShopName $ShopName -QueryString "tag:ShipTest" | Get-TervisShopifyOrdersForImport -ShopName $ShopName
 }
+
+function Get-TervisShopifyEBSRecord {
+  param (
+    [Parameter(Mandatory)]$OrigSysDocumentRef
+  )
+  
+  $Tables =   "xxoe_headers_iface_all",
+              "xxoe_customer_info_iface_all",
+              "xxoe_lines_iface_all"
+
+  foreach ($Table in $Tables) {
+    Write-Warning "$Table"
+    Invoke-EBSSQL -SQLCommand "SELECT * FROM $Table WHERE orig_sys_document_ref = '$OrigSysDocumentRef'"
+  }
+}
