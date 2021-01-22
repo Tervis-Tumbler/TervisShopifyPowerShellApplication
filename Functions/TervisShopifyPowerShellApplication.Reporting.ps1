4212cf82988a600e4012f88cb53b14aa9b42235e
Hamilton Perez <hambone124@users.noreply.github.com>
2021-01-22 08:38:33 -0500

    Get correct cred, remove cred fetch from loop

--- Functions/TervisShopifyPowerShellApplication.Reporting.ps1
+++ Functions/TervisShopifyPowerShellApplication.Reporting.ps1
@@ -3,6 +3,7 @@
 
 function Send-TervisShopifyReportingShopperTrakSalesData {
     $ReportContext = Get-TervisPasswordstatePassword -Guid "bbc460b6-d042-4a3a-ae03-9cc636d15de5"
+    $ShopperTrakSFTPCredential = Get-PasswordstatePassword -ID 6429 -AsCredential
     $Files = Get-ChildItem -Path "$($ReportContext.URL)\Inbound" -Filter SALES_*.txt
     foreach ($File in $Files) {
         $Data = $null
@@ -17,7 +18,6 @@ function Send-TervisShopifyReportingShopperTrakSalesData {
         $FilteredData = $Data | Where-Object {$_ -like "*$FilterString*"}
         $OutboundFile = "$($ReportContext.URL)\Outbound\$($File.Name)"
         $FilteredData | Out-File -FilePath $OutboundFile -Force -Encoding utf8 
-        $ShopperTrakSFTPCredential = Get-PasswordstatePassword -ID 6424 -AsCredential
         $ShopperTrakSFTPURL = "data.shoppertrak.com"
         $SFTPSession = New-SFTPSession -ComputerName $ShopperTrakSFTPURL -Credential $ShopperTrakSFTPCredential -AcceptKey
         Set-SFTPFile -SFTPSession $SFTPSession -LocalFile $OutboundFile -Verbose -RemotePath "/"
